2c402c46be26af608edba7898834dae0fb5d5620
Vyacheslav Lyubchich <vlyubchich@users.noreply.github.com>
2025-11-18 16:37:36 -0500

    Expand Annapolis data

--- R/data-Annapolis.R
+++ R/data-Annapolis.R
@@ -1,12 +1,12 @@
-#' Annapolis daily Daymet time series (1995-2024)
+#' Annapolis daily Daymet time series (1980-2024)
 #'
 #' Daily maximum temperature and precipitation for a single Daymet pixel near
-#' Annapolis, Maryland (USA). These data were retrieved via the package \code{daymetr}
+#' Annapolis, Maryland (USA). These data were retrieved via the package \code{daymetr} version 1.7.1
 #' and pared down for compact examples in this package.
 #' \itemize{
 #'   \item Source: Daymet (ORNL DAAC / NASA ESDIS) via the package \code{daymetr}.
 #'   \item Units: \code{tmax} in degrees Celsius; \code{pcp} in millimeters per day.
-#'   \item Coverage: \code{1995-01-01} through \code{2024-12-31} (inclusive; 365-day years, excluding 29 February), one location (Annapolis, MD; approx. 38.9784° N, 76.4922° W).
+#'   \item Coverage: \code{1980-01-01} through \code{2024-12-31} (inclusive; 365-day years excluding 31 December from leap years), one location (Annapolis, MD; approx. 38.9784° N, 76.4922° W).
 #' }
 #'
 #' @format A tibble (data frame) with the following columns:
@@ -16,9 +16,9 @@
 #'   \item \code{pcp} numeric. Daily total precipitation (mm/day).
 #' }
 #'
-#' @source Thornton, M. M., Shrestha, R., Wei, Y., Thornton, P. E., & Kao, S.-C. (2022). 
-#' Daymet: Daily Surface Weather Data on a 1-km Grid for North America, Version 4 R1 (Version 4.1). 
-#' ORNL Distributed Active Archive Center. \url{https://doi.org/10.3334/ORNLDAAC/2129} Date Accessed: 2025-10-31.
+#' @source Thornton, M. M., Shrestha, R., Wei, Y., Thornton, P. E., & Kao, S.-C. (2022).
+#' Daymet: Daily Surface Weather Data on a 1-km Grid for North America, Version 4 R1 (Version 4.1).
+#' ORNL Distributed Active Archive Center. \url{https://doi.org/10.3334/ORNLDAAC/2129} Date Accessed: 2025-11-18.
 #'
 #' @docType data
 #' @keywords datasets
